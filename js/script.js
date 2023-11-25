2b47d54bfe015b0519dc5420afff5e9ce2636666
Kodrillar <ipadeoladavid112@gmail.com>
2023-11-25 04:01:27 +0100

    UI:Add Frequently Asked Questions Section

--- js/script.js
+++ js/script.js
@@ -1,23 +1,58 @@
+/**
+ * Navigation Dropdown
+ */
 const navDropdownOptions = document.querySelector(".option").children;
 const navOptionDetails = document.querySelector(".option__details").children;
 
-document.querySelectorAll(".option__name").forEach((optionName) => {
-  const optionIndex = Array.from(navDropdownOptions).indexOf(optionName);
+function onNavDropdownOptionHover() {
+  document.querySelectorAll(".option__name").forEach((optionName) => {
+    const optionIndex = Array.from(navDropdownOptions).indexOf(optionName);
 
-  optionName.addEventListener("mouseover", function () {
-    Array.from(navDropdownOptions).forEach((option) => {
-      if (option.classList.contains("container--shadow")) {
-        option.classList.remove("container--shadow");
-      }
-    });
+    optionName.addEventListener("mouseover", function () {
+      Array.from(navDropdownOptions).forEach((option) => {
+        if (option.classList.contains("container--shadow")) {
+          option.classList.remove("container--shadow");
+        }
+      });
 
-    navDropdownOptions[optionIndex].classList.add("container--shadow");
-    navOptionDetails[optionIndex].classList.add("option--show");
+      navDropdownOptions[optionIndex].classList.add("container--shadow");
+      navOptionDetails[optionIndex].classList.add("option--show");
 
-    Array.from(navOptionDetails).forEach((optionDetail) => {
-      if (Array.from(navOptionDetails).indexOf(optionDetail) !== optionIndex) {
-        optionDetail.classList.remove("option--show");
-      }
+      Array.from(navOptionDetails).forEach((optionDetail) => {
+        if (
+          Array.from(navOptionDetails).indexOf(optionDetail) !== optionIndex
+        ) {
+          optionDetail.classList.remove("option--show");
+        }
+      });
     });
   });
-});
+}
+
+onNavDropdownOptionHover();
+
+/**
+ * FAQ Dropdown
+ */
+const faqDropdowns = document.querySelectorAll(".faq__dropdown");
+const faqAnswers = document.querySelectorAll(".faq__answer");
+const faqButtons = document.querySelectorAll(".faq__button");
+
+function onDropdownClicked() {
+  Array.from(faqDropdowns).forEach((faqDropdown) =>
+    faqDropdown.addEventListener("click", showFaqAnswer)
+  );
+}
+
+function showFaqAnswer(event) {
+  const faqAnswerList = Array.from(faqAnswers);
+  const faqDropdownList = Array.from(faqDropdowns);
+  const faqButtonList = Array.from(faqButtons);
+
+  const faqDropdownIndex = faqDropdownList.indexOf(this);
+
+  faqAnswerList[faqDropdownIndex].classList.toggle("faq--hide");
+  faqButtonList[faqDropdownIndex].classList.toggle("faq--rotate");
+}
+
+onDropdownClicked();
